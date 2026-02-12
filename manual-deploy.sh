2d0f3d14c57d38d594a7b535a0305301c6522a6d
Carlos <carlos.negron42@upr.edu>
2026-02-12 23:39:56 +0000

    Configure CI/CD

--- manual-deploy.sh
+++ manual-deploy.sh
@@ -1,6 +1,6 @@
 ### VARIABLES TO CHANGE - START
-PROJECT_ID=
-SERVICE_NAME=
+PROJECT_ID=carlos-negron-uprm
+SERVICE_NAME=group-6
 ### VARIABLES TO CHANGE - END
 
 # ----------- Manual Deployment ------------ #
