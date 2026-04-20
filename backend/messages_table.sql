4e6dadaf891581e70b3da4fc9aaf4e605a926263
Ayomide Ogunbowale <aogunbowale@pvamu.edu>
2026-04-20 19:32:05 +0000

    Add messages table SQL

--- backend/messages_table.sql
+++ backend/messages_table.sql
@@ -1,3 +1,11 @@
-"""
-For Ayomide to implement
-"""
+CREATE TABLE `carlos-negron-uprm.database.messages` (
+    message_id  STRING  NOT NULL,
+    sender_id   STRING  NOT NULL,
+    receiver_id STRING  NOT NULL,
+    content     STRING  NOT NULL,
+    timestamp   TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    `read`      BOOL    DEFAULT FALSE
+)
+PARTITION BY DATE(timestamp)
+CLUSTER BY sender_id, receiver_id
+OPTIONS (description = "Direct messages between users");
